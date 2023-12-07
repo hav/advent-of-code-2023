a8c51c1eb5c5b347d956ac50b30507b58ec63fe7
Farshid Besharati <farshid.besharati@volvocars.com>
2023-12-07 08:38:29 +0100

    day1 wip zzz...

--- day1/day1.go
+++ day1/day1.go
@@ -3,6 +3,7 @@ package day1
 import (
 	"fmt"
 	"regexp"
+	"sort"
 	"strconv"
 	"strings"
 )
@@ -18,12 +19,12 @@ var charToIntMap = map[string]string{
 	"eight": "8",
 	"nine":  "9",
 }
-var re = regexp.MustCompile("(one)|(two)|(three)|(four)|(five)|(six)|(seven)|(eight)|(nine)")
+var re = regexp.MustCompile("[0-9]|(one)|(two)|(three)|(four)|(five)|(six)|(seven)|(eight)|(nine)")
 
 func replaceAllStrings(input string, re *regexp.Regexp) string {
 	var tmpString = input
 	splitString := strings.Split(input, "")
-	matches := re.FindAllStringIndex(tmpString, 10)
+	matches := re.FindAllStringIndex(tmpString, -1)
 
 	for _, match := range matches {
 		stringToReplace := strings.Join(splitString[match[0]:match[1]], "")
@@ -31,6 +32,93 @@ func replaceAllStrings(input string, re *regexp.Regexp) string {
 
 	}
 	return tmpString
+}
+
+// func findFirstAndLastInt(input string) (int int) {
+// 	firstInt, lastInt := 0, 0
+// 	re := regexp.MustCompile(`\d+`)
+// 	matches := re.FindAllString(input, -1)
+// 	if len(matches) > 0 {
+// 		firstInt, lastInt := convertToInt(matches[len(matches)-1]), convertToInt(matches[0])
+// 	}
+// 	return firstInt, lastInt
+// }
+
+type WordMatch struct {
+	index int
+	word  string
+}
+
+func sortWordMatches(wordSet map[WordMatch]bool) []WordMatch {
+	words := []WordMatch{}
+	for k := range wordSet {
+		words = append(words, k)
+	}
+
+	sort.Slice(words, func(i, j int) bool {
+		return words[i].index < words[j].index
+	})
+
+	return words
+}
+
+func calibrationValues2(input []string, partTwo bool) int {
+	re := regexp.MustCompile("[0-9]")
+	keys := []string{"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"}
+
+	totalSum := 0
+	for _, line := range input {
+		firstIntIndex := 1000000
+		lastIntIndex := 0
+
+		if intIndexes := re.FindAllStringIndex(line, -1); intIndexes != nil {
+			firstIntIndex = intIndexes[0][0]
+			lastIntIndex = intIndexes[len(intIndexes)-1][0]
+		}
+
+		wordSet := map[WordMatch]bool{}
+
+		for _, v := range keys {
+			firstWordIndex := strings.Index(input[0], v)
+			lastWordIndex := strings.LastIndex(input[0], v)
+			if firstWordIndex != -1 {
+				wordSet[WordMatch{firstWordIndex, v}] = true
+			}
+
+			if lastWordIndex != -1 {
+				wordSet[WordMatch{lastWordIndex, v}] = true
+			}
+		}
+
+		words := sortWordMatches(wordSet)
+
+		firstIndex := firstIntIndex
+		lastIndex := lastIntIndex
+
+		var firstInt, lastInt int
+
+		if len(words) != 0 && words[0].index < firstIntIndex {
+			firstInt = convertToInt(charToIntMap[words[0].word])
+		} else {
+			firstInt = convertToInt(line[firstIndex : firstIndex+1])
+		}
+
+		if len(words) != 0 && words[len(words)-1].index > lastIntIndex {
+			lastInt = convertToInt(charToIntMap[words[len(words)-1].word])
+		} else {
+			lastInt = convertToInt(line[lastIndex : lastIndex+1])
+		}
+
+		// fmt.Println(firstInt*10 + lastInt)
+		totalSum += firstInt*10 + lastInt
+		// fmt.Println(firstInt, lastInt)
+		// fmt.Printf("index first number: %d, index last number: %d\n", firstIndex, lastIndex)
+		// fmt.Printf("first number: %s, last number: %s\n", line[firstIndex:firstIndex+1], charToIntMap[lastWord])
+
+		// totalSum += convertToInt(firstInt + lastInt)
+	}
+
+	return totalSum
 }
 
 func calibrationValues(input []string, regex string, partTwo bool) int {
--- day1/day1_test.go
+++ day1/day1_test.go
@@ -12,7 +12,9 @@ func TestDay1(t *testing.T) {
 		"a1b2c3d4e5f",
 		"treb7uchet"}
 
-	sumAll := calibrationValues(testInput, "[^0-9]", false)
+	// testInput := []string{"pqr3stu8vwx"}
+
+	sumAll := calibrationValues2(testInput, false)
 
 	if sumAll != 142 {
 		t.Errorf("was %d", sumAll)
@@ -22,34 +24,68 @@ func TestDay1(t *testing.T) {
 func TestDay1Real(t *testing.T) {
 	input := utils.ReadFileAsSlice("input.txt")
 
-	sumAll := calibrationValues(input, "[^0-9]", false)
+	// sumAll := calibrationValues(input, "[^0-9]", false)
+	sumAll := calibrationValues2(input, false)
 
 	if sumAll != 55621 {
 		t.Errorf("was %d", sumAll)
 	}
 }
 
-func TestDay12(t *testing.T) {
-	testInput := []string{"two1nine",
-		"eightwothree",
-		"abcone2threexyz",
-		"xtwone3four",
-		"4nineeightseven2",
-		"zoneight234",
-		"7pqrstsixteen",
-		"eight"}
+// func TestDay12(t *testing.T) {
+// 	testInput := []string{"two1nine",
+// 		"eightwothree",
+// 		"abcone2threexyz",
+// 		"xtwone3four",
+// 		"4nineeightseven2",
+// 		"zoneight234",
+// 		"7pqrstsixteen",
+// 		"vrrgrhnj1xgmcmd76two2oneightgqp"}
 
-	sumAll := calibrationValues(testInput, "[^0-9]", true)
+// 	var re = regexp.MustCompile("[0-9]")
+// 	intIndexes := re.FindAllStringIndex(testInput[0], -1)
 
-	if sumAll != 281 {
-		t.Errorf("was %d", sumAll)
-	}
-}
+// 	firstIndex := 1000000
+// 	lastIndex := 0
+// 	lastWord := ""
+
+// 	if firstIntIndex := intIndexes[0]; firstIntIndex != nil {
+// 		firstIndex = intIndexes[0][0]
+// 	}
+
+// 	if lastIntIndex := intIndexes[len(intIndexes)-1]; lastIntIndex != nil {
+// 		lastIndex = intIndexes[len(intIndexes)-1][0]
+// 	}
+
+// 	for _, v := range []string{"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"} {
+// 		if index := strings.LastIndex(testInput[0], v); index > -1 {
+// 			if index > lastIndex {
+// 				lastIndex = index
+// 				lastWord = v
+// 			}
+// 		}
+
+// 		if index := strings.Index(testInput[0], v); index > -1 {
+// 			if index < firstIndex {
+// 				firstIndex = index
+// 			}
+// 		}
+// 	}
+
+// 	fmt.Printf("index first number: %d, index last number: %d\n", firstIndex, lastIndex)
+// 	fmt.Printf("first number: %s, last number: %s\n", testInput[0][firstIndex:firstIndex+1], charToIntMap[lastWord])
+
+// 	// sumAll := calibrationValues(testInput, "[^0-9]", true)
+
+// 	// // if sumAll != 281 {
+// 	// if sumAll != 18 {
+// 	// 	t.Errorf("was %d", sumAll)
+// 	// }
 
 func TestDay1Part2Real(t *testing.T) {
 	input := utils.ReadFileAsSlice("input.txt")
 
-	sumAll := calibrationValues(input, "[^0-9]", true)
+	sumAll := calibrationValues2(input, true)
 
 	if sumAll != 53592 {
 		t.Errorf("was %d", sumAll)
