3556898951284650a26e810019fcc9b3ed0cccb7
Farshid Besharati <farshid.besharati@volvocars.com>
2023-12-22 17:29:06 +0100

    day3 part2

--- day3/day3.go
+++ day3/day3.go
@@ -16,6 +16,10 @@ type Part struct {
 	lastIndex  int
 }
 
+func (p Part) String() string {
+	return p.part
+}
+
 func (p Part) isPart(line string) (bool, string, []int) {
 	if symbol := symbolRegex.FindAllStringIndex(line, -1); symbol != nil {
 		for _, s := range symbol {
@@ -42,7 +46,6 @@ func part1(input []string) int {
 
 		for _, partIdxs := range potentialPartIdxs {
 			pp := Part{part: line[partIdxs[0]:partIdxs[1]], firstIndex: partIdxs[0], lastIndex: partIdxs[1]}
-			// fmt.Printf("Trying to identify if %s is a part number\n", pp.part)
 			inputIdxsToCheck := []int{inputIdx - 1, inputIdx, inputIdx + 1}
 
 			// figure out which lines to look for symbols in
@@ -65,8 +68,80 @@ func part1(input []string) int {
 	return partsSum
 }
 
-		}
+type Gear struct {
+	firstIndex int
+	lastIndex  int
+	lineIndex  int
+	parts      map[Part]bool
+}
+
+func (g Gear) calculateRatio() int {
+	keys := make([]Part, 0, len(g.parts))
+	for p := range g.parts {
+		keys = append(keys, p)
 	}
 
-	return partsSum
+	ratio := utils.ConvertToInt(keys[0].part) * utils.ConvertToInt(keys[1].part)
+	// fmt.Println("Gear", g, "has ratio", ratio)
+	return ratio
+}
+
+func createGear(gear []int, line string, inputIdx int) Gear {
+	startIndex := gear[0] - 1
+	endIndex := gear[1]
+	if gear[0] == 0 {
+		startIndex = 0
+	}
+
+	if gear[1] == len(line)-1 {
+		endIndex = len(line)
+	}
+	return Gear{firstIndex: startIndex, lastIndex: endIndex, parts: map[Part]bool{}, lineIndex: inputIdx + 1}
+}
+
+func (g Gear) isGearPart(firstIndex, lastIndex int) bool {
+	for i := g.firstIndex; i <= g.lastIndex; i++ {
+		if firstIndex == i || lastIndex-1 == i {
+			return true
+		}
+	}
+	return false
+}
+
+func part2(input []string) int {
+	symbolRegex := regexp.MustCompile(`([*])`)
+	totalRatio := 0
+	for inputIdx, line := range input {
+		gears := symbolRegex.FindAllStringIndex(line, -1)
+		for _, gear := range gears {
+			g := createGear(gear, line, inputIdx)
+			inputIdxsToCheck := []int{inputIdx - 1, inputIdx, inputIdx + 1}
+
+			// figure out which lines to look for symbols in
+			switch inputIdx {
+			case 0:
+				inputIdxsToCheck = inputIdxsToCheck[1:]
+			case len(input) - 1:
+				inputIdxsToCheck = inputIdxsToCheck[:len(inputIdxsToCheck)-1]
+			}
+
+			for _, inputLine := range inputIdxsToCheck {
+				potentialParts := numberRegex.FindAllStringIndex(input[inputLine], -1)
+				for _, part := range potentialParts {
+					if g.isGearPart(part[0], part[1]) {
+						pp := Part{part: input[inputLine][part[0]:part[1]], firstIndex: part[0], lastIndex: part[1]}
+						g.parts[pp] = true
+					}
+				}
+			}
+
+			if len(g.parts) == 2 {
+				// fmt.Printf("Gear at %v on line %d because we found parts %v close to it\n", []int{g.firstIndex, g.lastIndex}, inputIdx+1, g.parts)
+				totalRatio += g.calculateRatio()
+				// fmt.Println("Ratio is now", totalRatio)
+			}
+			// fmt.Println()
+		}
+	}
+	return totalRatio
 }
--- day3/day3_test.go
+++ day3/day3_test.go
@@ -6,31 +6,111 @@ import (
 	"besharati.se/advent-of-code-2023/utils"
 )
 
-func TestPart1TestInput(t *testing.T) {
-	input := []string{
-		"467..114..",
-		"...*......",
-		"..35..633.",
-		"......#...",
-		"617*......",
-		".....+.58.",
-		"..592.....",
-		"......755.",
-		"...$.*....",
-		".664.598.."}
-
-	partsSum := part1(input)
-
-	if partsSum != 4361 {
-		t.Errorf("was %d", partsSum)
+func TestPart1(t *testing.T) {
+	testCases := []struct {
+		desc     string
+		input    []string
+		expected int
+	}{
+		{
+			desc: "test",
+			input: []string{
+				"467..114..",
+				"...*......",
+				"..35..633.",
+				"......#...",
+				"617*......",
+				".....+.58.",
+				"..592.....",
+				"......755.",
+				"...$.*....",
+				".664.598.."},
+			expected: 4361,
+		},
+		{
+			desc:     "puzzle",
+			input:    utils.ReadFileAsSlice("input.txt"),
+			expected: 529618,
+		},
+	}
+	for _, tC := range testCases {
+		t.Run(tC.desc, func(t *testing.T) {
+			partsSum := part1(tC.input)
+			if partsSum != tC.expected {
+				t.Errorf("was %d", partsSum)
+			}
+		})
 	}
 }
 
-func TestPart1Input(t *testing.T) {
-	input := utils.ReadFileAsSlice("input.txt")
-	partsSum := part1(input)
+func TestPart2(t *testing.T) {
+	testCases := []struct {
+		desc     string
+		input    []string
+		expected int
+	}{
+		{
+			desc: "test",
+			input: []string{
+				"467..114..",
+				"...*......",
+				"..35..633.",
+				"......#...",
+				"617*......",
+				".....+.58.",
+				"..592.....",
+				"......755.",
+				"...$.*....",
+				".664.598.."},
+			expected: 467835,
+		},
+		{
+			desc:     "puzzle",
+			input:    utils.ReadFileAsSlice("input.txt"),
+			expected: 77509019,
+		},
+	}
+	for _, tC := range testCases {
+		t.Run(tC.desc, func(t *testing.T) {
+			partsSum := part2(tC.input)
+			if partsSum != tC.expected {
+				t.Errorf("was %d", partsSum)
+			}
 
-	if partsSum != 529618 {
-		t.Errorf("was %d", partsSum)
+		})
 	}
 }
+
+// func TestPart2TestInput(t *testing.T) {
+// 	input := []string{
+// 		"467..114..",
+// 		"...*......",
+// 		"..35..633.",
+// 		"......#...",
+// 		"617*......",
+// 		".....+.58.",
+// 		"..592.....",
+// 		"......755.",
+// 		"...$.*....",
+// 		".664.598.."}
+
+// 	// input := []string{
+// 	// 	"...*...",
+// 	// 	"432.997"}
+
+// 	partsSum := part2(input)
+
+// 	if partsSum != 467835 {
+// 		t.Errorf("was %d", partsSum)
+// 	}
+// }
+
+// func TestPart2Input(t *testing.T) {
+// 	input := utils.ReadFileAsSlice("input.txt")
+
+// 	partsSum := part2(input)
+
+// 	if partsSum != 70104392 {
+// 		t.Errorf("was %d", partsSum)
+// 	}
+// }
