8b14e8d6258e0e8d7c1478709d108153f1deff39
Farshid Besharati <farshid.besharati@volvocars.com>
2023-12-22 23:36:35 +0100

    day4 part2

--- day4/day4.go
+++ day4/day4.go
@@ -12,15 +12,16 @@ type Game struct {
 	id             int
 	winningNumbers []int
 	numbers        []int
+	count          int
 }
 
 var numbersRegex = regexp.MustCompile(`Card *(\d)+: *((?:[0-9]+ *)+) \| *((?:[0-9]+ *)+)`)
 
-func createGame(inputLine string) Game {
+func createGame(inputLine string) *Game {
 	numbers := numbersRegex.FindAllStringSubmatch(inputLine, -1)
 
 	id := utils.ConvertToInt(numbers[0][1])
-	game := Game{id: id}
+	game := Game{id: id, count: 1}
 
 	for _, number := range strings.Fields(numbers[0][2]) {
 		game.winningNumbers = append(game.winningNumbers, utils.ConvertToInt(number))
@@ -29,19 +30,29 @@ func createGame(inputLine string) Game {
 	for _, number := range strings.Fields(numbers[0][3]) {
 		game.numbers = append(game.numbers, utils.ConvertToInt(number))
 	}
-	return game
+	return &game
+}
+
+func (g Game) countMatches() int {
+	matches := 0
+
+	for _, number := range g.numbers {
+		if slices.Contains(g.winningNumbers, number) {
+			matches += 1
+		}
+	}
+
+	return matches
 }
 
 func (g Game) calculateWinnings() int {
 	winnings := 0
 
-	for _, number := range g.numbers {
-		if slices.Contains(g.winningNumbers, number) {
-			if winnings == 0 {
-				winnings += 1
-			} else {
-				winnings *= 2
-			}
+	for i := 1; i <= g.countMatches(); i++ {
+		if winnings == 0 {
+			winnings += 1
+		} else {
+			winnings *= 2
 		}
 	}
 
@@ -50,7 +61,7 @@ func (g Game) calculateWinnings() int {
 
 func part1(input []string) int {
 	winnings := 0
-	games := []Game{}
+	games := []*Game{}
 	for _, line := range input {
 		games = append(games, createGame(line))
 	}
@@ -60,4 +71,35 @@ func part1(input []string) int {
 	}
 
 	return winnings
+}
+
+func part2(input []string) int {
+	totalScratchcards := 0
+	games := []*Game{}
+	for _, line := range input {
+		games = append(games, createGame(line))
+	}
+
+	for idx, game := range games {
+		// fmt.Println("Adding", game.countMatches(), "for game", game.id, "to games after", idx)
+		for i := 0; i < game.count; i++ {
+			for i := 1; i <= game.countMatches(); i++ {
+				index := idx + i
+				if index > len(games)-1 {
+					index = len(games) - 1
+				}
+				// fmt.Println("Adding", 1, "to game at index", index, games[index])
+
+				games[index].count += 1
+			}
+		}
+		// fmt.Println()
+	}
+
+	for _, game := range games {
+		// fmt.Println(game)
+		totalScratchcards += game.count
+	}
+
+	return totalScratchcards
 }
--- day4/day4_test.go
+++ day4/day4_test.go
@@ -39,40 +39,35 @@ func TestPart1(t *testing.T) {
 	}
 }
 
-// func TestPart2(t *testing.T) {
-// 	testCases := []struct {
-// 		desc     string
-// 		input    []string
-// 		expected int
-// 	}{
-// 		{
-// 			desc: "testInput",
-// 			input: []string{
-// 				"467..114..",
-// 				"...*......",
-// 				"..35..633.",
-// 				"......#...",
-// 				"617*......",
-// 				".....+.58.",
-// 				"..592.....",
-// 				"......755.",
-// 				"...$.*....",
-// 				".664.598.."},
-// 			expected: 467835,
-// 		},
-// 		{
-// 			desc:     "puzzleInput",
-// 			input:    utils.ReadFileAsSlice("input.txt"),
-// 			expected: 77509019,
-// 		},
-// 	}
-// 	for _, tC := range testCases {
-// 		t.Run(tC.desc, func(t *testing.T) {
-// 			partsSum := part2(tC.input)
-// 			if partsSum != tC.expected {
-// 				t.Errorf("Error, was %d but should be %d", partsSum, tC.expected)
-// 			}
-
-// 		})
-// 	}
-// }
+func TestPart2(t *testing.T) {
+	testCases := []struct {
+		desc     string
+		input    []string
+		expected int
+	}{
+		{
+			desc: "testInput",
+			input: []string{
+				"Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53",
+				"Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19",
+				"Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1",
+				"Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83",
+				"Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36",
+				"Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"},
+			expected: 30,
+		},
+		{
+			desc:     "puzzleInput",
+			input:    utils.ReadFileAsSlice("input.txt"),
+			expected: 5422730,
+		},
+	}
+	for _, tC := range testCases {
+		t.Run(tC.desc, func(t *testing.T) {
+			partsSum := part2(tC.input)
+			if partsSum != tC.expected {
+				t.Errorf("Error, was %d but should be %d", partsSum, tC.expected)
+			}
+		})
+	}
+}
